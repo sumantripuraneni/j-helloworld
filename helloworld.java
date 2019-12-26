ac791eb5d591824a654a25c3876a9557915fac21
sumantripuraneni <36487778+sumantripuraneni@users.noreply.github.com>
2019-12-25 19:26:16 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
  public static void main(String[] args){
-   System.out.println("****Hello world Java program, from webhook2");
+   System.out.println("****Hello world Java program, from webhook");
 }
 }
